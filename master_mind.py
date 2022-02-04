fdaadfdb1978e791e64099f74052a071713f50e0
luffy981 <jhonsmith981@hotmail.com>
2022-02-04 18:40:44 -0500

    More validations to master mind

--- master_mind.py
+++ master_mind.py
@@ -4,15 +4,27 @@ import random
 from colorama import Fore,  Style
 from art import *
 
+dict_faces = {
+    "0": art("kissing"),
+    "1": art("happy hug"),
+    "2": art("meow"),
+    "3": art("worried"),
+    "4": art("confused7"),
+    "5": art("crazy"),
+    "6": art("angry1"),
+    "7": art("exorcism")
+ }
+colors = ["green", "red", "blue", "yellow", "magenta",
+          "black", "white", "cyan"]
+colora = [Fore.GREEN, Fore.RED, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA,
+          Fore.BLACK, Fore.WHITE, Fore.CYAN]
+keys = ["1", "2", "3", "4", "5", "6", "7", "8"]
+dict_val = dict(zip(keys, colors))
+dict_colors = dict(zip(colors, colora))
+dict_keys = dict(zip(colors, keys))
 
-if __name__ == "__main__":
-    colors = ["green", "red", "blue", "yellow", "magenta",
-              "black", "white", "cyan"]
-    colora = [Fore.GREEN, Fore.RED, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA,
-              Fore.BLACK, Fore.WHITE, Fore.CYAN]
-    keys = ["1", "2", "3", "4", "5", "6", "7", "8"]
-    dict_val = dict(zip(keys, colors))
-    dict_colors = dict(zip(colors, colora))
+
+def mastermind():
     while (True):
         art_2 = art("happy")
         try:
@@ -30,21 +42,14 @@ if __name__ == "__main__":
     switcher = {"1": 4, "2": 6, "3": 8}
     balls = switcher[str(difficult)]
     secret_list = random.sample(colors, k=balls)
+    print(*secret_list)
     print("*****Please choose {} colors bettwen: *****\n".format(balls))
     art_2 = art("happy")
     print("                        {}\n".format(art_2))
-    flag = 0
+    to_list = list(dict(enumerate(colors)).keys()) 
+    to_list = list(map(lambda x: x + 1, to_list))
+    print_colors(colors, to_list)
     for k in range(0, 8):
-        n = 1;
-        for color in colors:
-            a = dict_colors.get(color)
-            print(a, end="")
-            if flag == 1:
-                print(", ", end="")
-            print("{}: {}".format(n, dict_val[str(n)] ), end="")
-            flag = 1
-            n += 1
-            print(Style.RESET_ALL, end="")
         print()
         try:
             gamer_list = []
@@ -54,21 +59,12 @@ if __name__ == "__main__":
                     raise Exception("Try again ...")
                 gamer_list.append(a)
         except Exception:
-            dict_faces = {
-                "0": art("kissing"),
-                "1": art("happy hug"),
-                "2": art("meow"),
-                "3": art("worried"),
-                "4": art("confused7"),
-                "5": art("crazy"),
-                "6": art("angry1"),
-                "7": art("exorcism")
-            }
             art_2 = dict_faces["{}".format(k)]
             print("try again ... {}".format(art_2))
             continue
         print(gamer_list)
         i = 0
+        validate = []
         for color in gamer_list:
             if dict_val[color] == secret_list[i]:
                 print(Fore.RED, end="")
@@ -84,9 +80,51 @@ if __name__ == "__main__":
             elif dict_val[color] not in secret_list:
                 Art = text2art("0")
                 print(Art, end="")
+            validate.append(dict_val[color])
             i += 1
+        if secret_list == validate:
+            Art = text2art("Congratulations...!")
+            print(Art)
+            art_2 = dict_faces["2"]
+            print("Do you want play again ...? {}".format(art_2))
+            answer = str(input(">>>  "))
+            if answer == "yes":
+                mastermind()
+            else:
+                print("Thanks for play ...! {}".format(art_2))
+                exit(0)
+        art_2 = dict_faces["{}".format(k)]
+        print("***** You chose this *****")
+        validate_keys = [dict_keys.get(x) for x in validate if dict_keys.get(x)]
+        print("try again ... {}".format(art_2))
+
+    print("***** You chose this *****")
+    validate_keys = [dict_keys.get(x) for x in validate if dict_keys.get(x)]
+    print_colors(validate, validate_keys)
+    print("***** The correct answer was *****")
+    secret_keys = [dict_keys.get(x) for x in secret_list if dict_keys.get(x)]
+    print_colors(secret_list, secret_keys)
+
     print()
     art_2=art("sad and crying")
     print("                        {}".format(art_2))
     Art = text2art("Game Over")
     print(Art, end="")
+    exit(0)
+
+
+def print_colors(list_color, list_gamer):
+    """Function to print colors"""
+    n = 0
+    flag = 0
+    for color in list_color:
+        if flag == 1:
+            print(", ", end="")
+        print(dict_colors[list_color[n]], end="")
+        print("{}: {}".format(list_gamer[n], list_color[n]), end="")
+        flag = 1
+        n += 1
+        print(Style.RESET_ALL, end="")
+    print()
+
+mastermind()
